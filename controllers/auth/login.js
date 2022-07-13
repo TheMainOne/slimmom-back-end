540b3390c9b82bd5d09c516a458ff2b6bbdece99
alena-senkiv <alena.senkiv@gmail.com>
2022-07-13 16:14:26 +0300

    fix login response

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -14,7 +14,7 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const { name } = user;
+  const { name, userData } = user;
   const token = jwt.sign(
     payload,
     SECRET_KEY
@@ -29,6 +29,7 @@ const login = async (req, res) => {
       user: {
         name,
         email,
+        userData,
       },
     },
   });
