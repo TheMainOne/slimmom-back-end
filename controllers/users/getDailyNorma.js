3d751a167ccd322401cd685b17c3fa51b9dfe60f
Yuliia-Lysiuk <84859026+Yuliia-Lysiuk@users.noreply.github.com>
2022-07-16 14:01:12 +0300

    save user data

--- controllers/users/getDailyNorma.js
+++ controllers/users/getDailyNorma.js
@@ -1,15 +1,10 @@
-const {
-  calcDailyNormKkal,
-  putPropsToUserNextReq,
-  componentBunnedProduct,
-} = require('../../helpers');
+const { calcDailyNormKkal, componentBunnedProduct } = require('../../helpers');
 
-const privateR = async ({ body: userData }, res, next) => {
+const privateR = async (req, res, next) => {
+  const { body: userData } = req;
   const dailyRate = calcDailyNormKkal(userData);
   const bannedProducts = await componentBunnedProduct(userData.bloodType);
-
-  putPropsToUserNextReq(userData, { dailyRate, bannedProducts });
-
+  req.user.userData = { ...userData, dailyRate, bannedProducts };
   res.json({
     status: 'success',
     code: 200,
