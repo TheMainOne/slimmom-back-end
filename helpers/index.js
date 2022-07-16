358c1eb6835e22ee42184ed98c4c522dfefe6d9a
Maksym Lvov <leva880@ukr.net>
2022-07-16 18:47:30 +0300

    fixed server cors problem

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,5 @@
 const calcDailyNormKkal = require("./calcDailyNormKkal");
 const findProductByBlood = require("./findProductByBlood");
-const putPropsToUserNextReq = require("./putPropsToUserNextReq");
 const reportProductByCategories = require("./reportProductByCategories");
 const componentBunnedProduct = require("./componentBunnedProduct");
 const diary = require("./diary");
@@ -8,7 +7,6 @@ const diary = require("./diary");
 module.exports = {
   calcDailyNormKkal,
   findProductByBlood,
-  putPropsToUserNextReq,
   reportProductByCategories,
   componentBunnedProduct,
   diary,
